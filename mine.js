4de6aea2bbbf1ba4c1ff8e30070f41b2209b731f
Joanne <joannesoeunhong@gmail.com>
2018-01-11 19:28:11 -0500

    before mining make sure there is an account

--- mine.js
+++ mine.js
@@ -1,10 +1,13 @@
-if (web3.eth.getBlock("pending").transactions.length > 0) {
-  if (web3.eth.mining) {
-    return;
-  }
+// Create an account if there isn't one yet
+if (web3.eth.accounts.length == 0) {
+  personal.newAccount("");
+}
 
-  console.log("Incoming pending transactions ... mining ...");
-  miner.start(1);
+if (web3.eth.getBlock("pending").transactions.length > 0) {
+  if (!web3.eth.mining) {
+    console.log("Incoming pending transactions ... mining ...");
+    miner.start(1);
+  }
 } else {
   console.log("Finished pending transactions ... stop mining ...");
   miner.stop();
